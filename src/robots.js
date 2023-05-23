d1286a795ea9d6c44f1c29bf021d9833e7505301
azimchamran <m.azims1396@gmail.com>
2023-05-23 11:58:42 +0330

    robots card created

---
+++ src/robots.js
@@ -0,0 +1,51 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Ali Reza',
+		username: 'Reza',
+		email: 'ali@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Ali Mohammad',
+		username: 'Mohammad',
+		email: 'mohammad@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Fahim',
+		username: 'Fahim',
+		email: 'fahim@gmail.com'
+	},
+	{
+		id: 4,
+		name: ' Ramazan',
+		username: 'Ramazan',
+		email: 'remazan@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Ahamad',
+		username: 'Ahmad',
+		email: 'ahmad@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Rahim',
+		username: 'Rahim',
+		email: 'rahim@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Afshan',
+		username: 'Afshan',
+		email: 'Afshan@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Ali Khan',
+		username: 'khan',
+		email: 'khan@gmail.com'
+	},
+
+]
