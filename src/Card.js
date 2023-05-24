26f05d88ba475e2b9c67d5822287da920cd61cf0
azimchamran <m.azims1396@gmail.com>
2023-05-24 16:05:19 +0330

    search box created for searching of the robots

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Card = (props) => {
-	const { name, email, id, username } = props;
+	const { name, email, id } = props;
 	return (
 		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
 			<img alt='photo' src = {`https://robohash.org/${props.id}?200x200`} />
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import CardList from  './CardList';
-import {robots} from './robots';
+import App from  './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <CardList robots={robots}/>
-    
+   <App />
   </React.StrictMode>
 );
 
