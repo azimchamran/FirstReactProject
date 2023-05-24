edab9052e84442b138c5eb4afa6875343dc9aecc
azimchamran <m.azims1396@gmail.com>
2023-05-24 16:46:38 +0330

    style of the application changed to new one

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 		})
 		return (
 			<div className='tc'>
-				<h1> RobotFriends</h1>
+				<h1 className='f1'> RobotFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<CardList robots={filteredRobots}/> 
 			</div>
